d151f8749bfe4031420035d62e53ef0735568d2e
Alex Dolara <jandry@gmail.com>
2021-05-13 16:14:42 +0200

    Image update. Deleting images.

--- js/random.js
+++ js/random.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var classCycle=['img1','img2','img3','img4','img5','img6','img7','img7','img8','img9','img10','img11','img12','img13'];
+    var classCycle=['img1','img2','img3','img4','img5','img6','img7','img7'];
 
     var randomNumber = Math.floor(Math.random() * classCycle.length);
     var classToAdd = classCycle[randomNumber];
