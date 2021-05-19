09a6371b623f1b1ddd7c236b92c8d15090e5c30e
Alex Dolara <jandry@gmail.com>
2021-05-19 14:55:40 +0200

    rollback

--- js/random.js
+++ js/random.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var classCycle=['img1','img2'];
+    var classCycle=['img1'];
     //,'img2','img3','img4','img5','img6','img7'
     var randomNumber = Math.floor(Math.random() * classCycle.length);
     var classToAdd = classCycle[randomNumber];
